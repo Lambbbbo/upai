95b2f85a30c52eab1d7a8f75224e9d84c7daf59d
jian.he <jian100328823@qq.com>
2020-03-12 14:20:30 +0800

    add

---
+++ myself_file/playbook/init-playbook/library/fetch_var.py
@@ -0,0 +1,109 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+import copy
+from ansible.module_utils.basic import AnsibleModule
+
+ENERU_API = "http://eneru.x.upyun.com/v1/inventory/"
+ApiKey = "ertsBIgtWqgieH4gdHzOMrthRjZUsuGh"
+
+class get_var(object):
+	def __init__(self, module, metadata):
+		self.module = module
+		self.facts = metadata
+		self.hostid = self.module.params["hostid"]
+		self.clusterid = self.module.params["clusterid"]
+		self.cluster_API = ENERU_API + "clusters/" + self.clusterid + "/hosts"
+
+	def upstream(self):
+		facts = {}
+		host_list = self.hostid.split(",")
+		headers = {"x-eneru-apikey": ApiKey}
+		req_allhost = requests.get(self.cluster_API, headers=headers)
+		
+		p_ip = []
+		for host in host_list:
+			host_API = ENERU_API + "hosts/" + host
+			req_host = requests.get(host_API, headers=headers)
+			res = json.loads(req_host.content)
+			p_ip.append(res["private_ipaddrs"])
+		facts["target_host"] = p_ip 
+		
+		result = json.loads(req_allhost.content)
+		p_ips = []
+		for host in result["items"]:
+			private_ip = host["private_ipaddrs"]
+			p_ips.append(private_ip)					#获取当前集群内机器的私网ip
+		
+		NGINX_UPSTREAM = copy.copy(p_ips)
+		for i in p_ip:	
+			if i in p_ips:
+				NGINX_UPSTREAM.remove(i)					#判断当target_host已在集群内，则移除target_host，生成upstream推送配置
+			else:
+				NGINX_UPSTREAM.append(i)					#判断当target_host不在集群内，则新增target_host，生成upstream推送配置
+			
+		facts["NGINX_UPSTREAM"] = NGINX_UPSTREAM
+
+		return facts
+	
+	def lvs(self):
+		facts = {}
+		host_list = self.hostid.split(",")	
+		headers = {"x-eneru-apikey": ApiKey}
+		cluster_info = ENERU_API + "clusters/" + self.clusterid
+		req_vip = requests.get(cluster_info, headers=headers)
+		
+		pub_ips = []
+		for host in host_list:
+			host_API = ENERU_API + "hosts/" + host
+			req_host = requests.get(host_API, headers=headers)
+			res = json.loads(req_host.content)
+			for public_ip in res["public_ipaddrs"]:
+				pub_ips.append(public_ip["addr"]) 		#获取target_host的公网ip
+		facts["PUBLIC_IP"] = pub_ips 		  	
+		
+		req_hosts = requests.get(self.cluster_API, headers=headers)
+		result = json.loads(req_hosts.content)
+		r_ips = []
+		
+		for host in result["items"]:
+			for h in host["public_ipaddrs"]:
+				r_ips.append(h["addr"])			#获取当前集群内机器的公网ip
+		facts["REAL_HOSTS"] = r_ips
+		
+		res_vip = json.loads(req_vip.content)
+		vip_list = {}
+		for vip in res_vip["public_ipaddrs"]:
+			vip_list[vip["addr"]] = vip["netmask"]
+		facts["VIP"] = vip_list
+		
+		return facts
+			
+		
+class Fetch(object):
+	def __init__(self, module):
+		self.module = module
+		self.facts = {}
+		self.action = self.module.params["action"]
+		
+	def run(self):
+		meta = get_var(self.module, self.facts)
+		if hasattr(meta, self.action):
+			self.facts.update(**getattr(meta, self.action)())
+
+
+def main():
+	module = AnsibleModule(argument_spec=dict(
+		hostid=dict(required=True),
+		clusterid=dict(required=True),
+		action=dict(required=True)))
+	
+	fetch = Fetch(module)
+	fetch.run()
+
+	module.exit_json(changed=False, ansible_facts=fetch.facts)
+
+if __name__ == '__main__':
+	main()
